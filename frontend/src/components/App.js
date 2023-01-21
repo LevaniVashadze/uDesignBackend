277f6adabdb3ec9f5a64fb785fb1eb1083f9da88
LevaniVashadze <vashadzelevani11@gmail.com>
2023-01-21 22:46:31 +0400

    add code splitting

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,24 +1,28 @@
-import React, { useEffect } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
-import AboutPage from "./AboutPage";
-import ContactPage from "./ContactPage";
-import ProductPage from "./ProductPage";
-import ProductsPage from "./ProductsPage";
 import LanguageContext from "../context/LanguageContext";
 import ThemeContext from "../context/ThemeContext";
+
+const AboutPage = lazy(() => import("./AboutPage"));
+const ContactPage = lazy(() => import("./ContactPage"));
+const ProductsPage = lazy(() => import("./ProductsPage"));
+const ProductPage = lazy(() => import("./ProductPage"));
 
 const App = () => {
   const [language, setLanguage] = React.useState("en");
   const [theme, setTheme] = React.useState(() => {
-      if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        return 'dark';
-      } else {
-        return 'light';
-      }
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      return "dark";
+    } else {
+      return "light";
     }
-  );
+  });
 
   useEffect(() => {
     setLanguage(window.localStorage.getItem("language"));
@@ -42,15 +46,17 @@ const App = () => {
     <div className="p-0 m-o">
       <LanguageContext.Provider value={[language, setLanguage]}>
         <ThemeContext.Provider value={[theme, setTheme]}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/products/:id" element={<ProductPage />} />
-              <Route path="/products" element={<ProductsPage />} />
-            </Routes>
-          </BrowserRouter>
+          <Suspense fallback={<div>Loading ...</div>}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/products/:id" element={<ProductPage />} />
+                <Route path="/products" element={<ProductsPage />} />
+              </Routes>
+            </BrowserRouter>
+          </Suspense>
         </ThemeContext.Provider>
       </LanguageContext.Provider>
     </div>
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
 def index(request, *args, **kwargs):
-    return render(request, "frontend/index.html")
+    filenames = settings.STATICFILES_DIRS
+    # might break with collectstatic
+    paths = [filename for filename in filenames if not filename.endswith(".txt")]
+    return render(request, "frontend/index.html", context={"paths": paths})
