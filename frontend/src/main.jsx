3407b476e8c52fd1b8894162900a73ef946d290b
LevaniVashadze <vashadzelevani11@gmail.com>
2023-06-26 21:49:28 +0400

    remove frontend

--- frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM, { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
-
-import "./i18n";
-
-const root = createRoot(document.getElementById("app"));
-root.render(<App />);
